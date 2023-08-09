314ce6a74b29d7a94cc6aa512a8374bf95a942da
Martin Lora <martinlorajorge@gmail.com>
2023-08-09 17:32:48 -0300

    json config standard

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,12 @@ export function App () {
       <h1>App de gatitos</h1>
 
       <button onClick={handleClick}>Get new fact</button>
-
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first rhee words for ${fact}`} />}
+      {imageUrl && (
+        <img
+          src={imageUrl} alt='Image extracted using the first rhee words for $fact}'
+        />
+      )}
     </main>
   )
 }
--- src/services/facts.js
+++ src/services/facts.js
@@ -1,5 +1,6 @@
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
+// Obtengo un echo aleatorio
 export const getRandomFact = async () => {
   const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
   const data = await res.json()
